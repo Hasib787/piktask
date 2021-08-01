7bfd2f4a59b427fcd25dfcd5b43e9d9087294dff
Pranto Shikder <48595837+prantoshikder@users.noreply.github.com>
2021-08-01 18:19:05 +0600

    Added a button design

--- src/pages/Home/Home.styles.jsx
+++ src/pages/Home/Home.styles.jsx
@@ -3,14 +3,16 @@ import { makeStyles } from "@material-ui/core/styles";
 const useStyles = makeStyles((theme) => ({
   headingButton: {
     ...theme.typography.button,
-    // backgroundColor: theme.palette.secondary.main,
-    padding: "0.8rem 3.5rem",
-    fontSize: "1.8rem",
+    backgroundColor: "rgba(0, 0, 0, 0.04)",
+    padding: "0.8rem 3rem",
+    fontSize: "1.7rem",
     fontWeight: 500,
     color: "#1B3F4E",
+    transition: "all 0.3s linear",
 
     "&:hover": {
-      backgroundColor: "transparent",
+      backgroundColor: "#117A00",
+      color: "#fff",
     },
   },
 }));
