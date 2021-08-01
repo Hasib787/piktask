42500846199774452ac9420816d1d89422bb00bc
Pranto Shikder <48595837+prantoshikder@users.noreply.github.com>
2021-08-01 11:34:54 +0600

    Design progress

--- src/pages/NotFoundPage/index.tsx
+++ src/pages/NotFoundPage/index.tsx
@@ -1,5 +1,17 @@
+import { Container } from "@material-ui/core";
 import React from "react";
+import useStyles from "./NotFoundPage.styles"
 
 export const NotFoundPage = () => {
-  return <div>Not found</div>;
+  const classes = useStyles();
+  return (
+    <>
+      <div className={classes.pageNotFound}>
+        <Container>
+          <h3>404</h3>
+          <h1>Page Not Found</h1>
+        </Container>
+      </div>
+    </>
+  )
 };
