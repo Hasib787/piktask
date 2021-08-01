a3857a9978243f0314dbdec1eac852e0f7c29a24
Pranto Shikder <48595837+prantoshikder@users.noreply.github.com>
2021-08-01 11:34:26 +0600

    Heading button background changes

--- src/pages/Home/Home.styles.jsx
+++ src/pages/Home/Home.styles.jsx
@@ -3,13 +3,14 @@ import { makeStyles } from "@material-ui/core/styles";
 const useStyles = makeStyles((theme) => ({
   headingButton: {
     ...theme.typography.button,
-    backgroundColor: theme.palette.secondary.main,
+    // backgroundColor: theme.palette.secondary.main,
     padding: "0.8rem 3.5rem",
-    fontSize: "1.5rem",
+    fontSize: "1.8rem",
     fontWeight: 500,
+    color: "#1B3F4E",
 
     "&:hover": {
-      backgroundColor: theme.palette.primary.main,
+      backgroundColor: "transparent",
     },
   },
 }));
