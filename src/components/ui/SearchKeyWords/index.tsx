670a7a8382ed088e6e031ddd1ee10990b7a76110
Pranto Shikder <48595837+prantoshikder@users.noreply.github.com>
2021-08-05 16:57:08 +0600

    Added popular API integration

--- src/components/ui/SearchKeyWords/index.tsx
+++ src/components/ui/SearchKeyWords/index.tsx
@@ -1,67 +1,55 @@
 import { Typography } from '@material-ui/core';
-import React, { FC } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 import useStyles from "./SearchKeyWords.styles";
 import {PropTypes} from "../Hero";
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 
 const SearchKeyWords: FC<PropTypes> = (props): JSX.Element => {
-    const classes = useStyles();
-    const { popularKeywords, creativeWorksDone } = props;
-    return (
-        <>
-          {popularKeywords && (
-            <div className={classes.popularSearch}>
-              <Typography variant="h5" className={classes.searchTitle}>
-                Example :  
-              </Typography>
-              <Link to={"/banner"}>
-                <Typography variant="h5" className={classes.searchTitle}>
-                  Banner, 
-                </Typography>
-              </Link>
-              <Link to={"/background"}>
-                <Typography variant="h5" className={classes.searchTitle}>
-                  Background, 
-                </Typography>
-              </Link>
-              <Link to={"/abstract Banner"}>
-                <Typography variant="h5" className={classes.searchTitle}>
-                  Abstract Banner, 
-                </Typography>
-              </Link>
-              <Link to={"/logo Design"}>
-                <Typography variant="h5" className={classes.searchTitle}>
-                  Logo Design, 
-                </Typography>
-              </Link>
-              <Link to={"/business card"}>
-                <Typography variant="h5" className={classes.searchTitle}>
-                  Business card, 
-                </Typography>
-              </Link>
-              <Link to={"/post Card Design"}>
-                <Typography variant="h5" className={classes.searchTitle}>
-                  Post Card Design, 
-                </Typography>
-              </Link>
-              <Link to={"/web template"}>
-                <Typography variant="h5" className={classes.searchTitle}>
-                  web template 
-                </Typography>
-              </Link>
-            </div>
-          )}
+  const classes = useStyles();
+  const { popularKeywords, creativeWorksDone } = props;
+  const [popularSearchKeywords, setPopularSearchKeywords] = useState([]);
 
-          {creativeWorksDone && (
-            <div className={classes.popularSearch}>
+  useEffect(() => {
+    try {
+      axios
+      .get(`${process.env.REACT_APP_API_URL}/client/search/popular_keyword`)
+      .then(({data}) => {
+        if (data?.status) {
+          setPopularSearchKeywords(data.keywords);
+        }
+      })
+    } catch (error) {
+      
+    }
+  }, [])
+
+  return (
+    <>
+      {popularKeywords && 
+        <div className={classes.popularSearch}>
+          <Typography variant="h5" className={classes.searchTitle}>
+            Example :  
+          </Typography>
+          {popularSearchKeywords?.map((keyWord, index) => (
+            <Link key={keyWord.index} to={`/tag/${keyWord}`}>
               <Typography variant="h5" className={classes.searchTitle}>
-                Over 500,000 creative templates help you get your work done in minutes!
+                {keyWord},
               </Typography>
-            </div>
-          )}
-        </>
-    );
+            </Link>))}
+        </div>
+      }
+      
+      {creativeWorksDone && (
+        <div className={classes.popularSearch}>
+          <Typography variant="h5" className={classes.searchTitle}>
+            Over 500,000 creative templates help you get your work done in minutes!
+          </Typography>
+        </div>
+      )}
+    </>
+  );
 };
 
 export default SearchKeyWords;
