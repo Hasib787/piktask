4faf69791c6c91d6862b2542b170ad793f67449a
Md. Masud Rana <mr.hasib45@gmail.com>
2021-08-16 15:11:24 +0600

    implement product by extension api

--- src/components/ui/AuthorItems/index.tsx
+++ src/components/ui/AuthorItems/index.tsx
@@ -12,8 +12,6 @@ const AuthorItems = ({ imageSummery, id }) => {
   const [value, setValue] = useState(0);
   const [isLoading, setLoading] = useState(true);
   const [authorAllResource, setAuthorAllResource] = useState([0]);
-  // const [imageExtension, setImageExtension] = useState();
-  console.log("imageSummary", imageSummery[0]?.extension);
 
   const handleActiveButton = (e: ChangeEvent<{}>, index: number) => {
     setValue(index);
@@ -23,34 +21,35 @@ const AuthorItems = ({ imageSummery, id }) => {
     if (imageSummery[0]?.extension !== undefined) {
       try {
         axios
-        .get(`${process.env.REACT_APP_API_URL}/user/${id}/images/${imageSummery[0]?.extension}`)
-        .then(({ data }) => {
-          if (data?.status) {
-            setAuthorAllResource(data?.images);
-            setLoading(false);
-          }
-        })
+          .get(
+            `${process.env.REACT_APP_API_URL}/user/${id}/images/${imageSummery[0]?.extension}`
+          )
+          .then(({ data }) => {
+            if (data?.status) {
+              setAuthorAllResource(data?.images);
+              setLoading(false);
+            }
+          });
       } catch (error) {
         console.log("All author resources", error);
       }
     } else {
       console.log("Sorry no extension found");
     }
-  }, [ id, imageSummery ])
+  }, [id, imageSummery]);
 
   const handleAuthorResource = (tag) => {
-
     if (tag !== undefined) {
       try {
         axios
-        .get(`${process.env.REACT_APP_API_URL}/user/${id}/images/${tag}`)
-        .then(({ data }) => {
-          if (data?.status) {
-            setAuthorAllResource(data?.images);
-            history.push(`/author/${id}/${tag}`);
-            setLoading(false);
-          }
-        })
+          .get(`${process.env.REACT_APP_API_URL}/user/${id}/images/${tag}`)
+          .then(({ data }) => {
+            if (data?.status) {
+              setAuthorAllResource(data?.images);
+              history.push(`/author/${id}/${tag}`);
+              setLoading(false);
+            }
+          });
       } catch (error) {
         console.log("All author resources", error);
       }
@@ -61,57 +60,59 @@ const AuthorItems = ({ imageSummery, id }) => {
 
   return (
     <Layout>
-    <Container>
-      <Grid container className={classes.authorItemTags}>
-        <Tabs
-          value={value}
-          onChange={handleActiveButton}
-          aria-label="Author item count"
-          classes={{
-            root: classes.root,
-            flexContainer: classes.flexContainer,
-            indicator: classes.indicator,
-          }}
-        >
-          {imageSummery.length > 0 &&
-            imageSummery.map((tag, index) => (
-              <Tab
-                key={index}
-                label={`${tag.extension} (${tag.images})`}
-                className={classes.tagButton}
-                classes={{ selected: classes.selected }}
-                onClick={() => handleAuthorResource(tag.extension)}
-              />
-            ))}
-        </Tabs>
-      </Grid>
+      <Container>
+        <Grid container className={classes.authorItemTags}>
+          <Tabs
+            value={value}
+            onChange={handleActiveButton}
+            aria-label="Author item count"
+            classes={{
+              root: classes.root,
+              flexContainer: classes.flexContainer,
+              indicator: classes.indicator,
+            }}
+          >
+            {imageSummery.length > 0 &&
+              imageSummery.map((tag, index) => (
+                <Tab
+                  key={index}
+                  label={`${tag.extension} (${tag.images})`}
+                  className={classes.tagButton}
+                  classes={{ selected: classes.selected }}
+                  onClick={() => handleAuthorResource(tag.extension)}
+                />
+              ))}
+          </Tabs>
+        </Grid>
 
-      <Grid classes={{ container: classes.container }} container spacing={2}>
-        {isLoading ? (
-          <h2>Loading now......</h2>
-        ) : (
-          <>
-            {/* {authorAllResource.filter((index) => authorAllResource === index.tag) && authorAllResource?.length ? ( */}
-            {authorAllResource?.length ? (
-              authorAllResource?.map((photo) => (
-                <Grid
-                  key={photo.image_id}
-                  item
-                  xs={6}
-                  sm={4}
-                  md={3}
-                  className={classes.productItem}
-                >
-                  <Product photo={photo} />
-                </Grid>
-              ))
-            ) : (
-              <Typography variant="body1">Sorry, no products found</Typography>
-            )}
-          </>
-        )}
-      </Grid>
-    </Container>
+        <Grid classes={{ container: classes.container }} container spacing={2}>
+          {isLoading ? (
+            <h2>Loading now......</h2>
+          ) : (
+            <>
+              {/* {authorAllResource.filter((index) => authorAllResource === index.tag) && authorAllResource?.length ? ( */}
+              {authorAllResource?.length ? (
+                authorAllResource?.map((photo) => (
+                  <Grid
+                    key={photo.image_id}
+                    item
+                    xs={6}
+                    sm={4}
+                    md={3}
+                    className={classes.productItem}
+                  >
+                    <Product photo={photo} />
+                  </Grid>
+                ))
+              ) : (
+                <Typography variant="body1">
+                  Sorry, no products found
+                </Typography>
+              )}
+            </>
+          )}
+        </Grid>
+      </Container>
     </Layout>
   );
 };
