63132efda833f05a7ff4221e2f3a945173b4f9e6
Md. Masud Rana <mr.hasib45@gmail.com>
2021-08-17 20:10:30 +0600

    added new component and implement resent api

---
+++ src/components/ui/Recent/Recent.style.js
@@ -0,0 +1,19 @@
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  headingButton: {
+    ...theme.typography.button,
+    backgroundColor: "rgba(0, 0, 0, 0.04)",
+    padding: "0.8rem 3rem",
+    fontSize: "1.7rem",
+    fontWeight: 500,
+    color: "#1B3F4E",
+    transition: "all 0.3s linear",
+
+    "&:hover": {
+      backgroundColor: "#117A00",
+      color: "#fff",
+    },
+  },
+}));
+export default useStyles;
