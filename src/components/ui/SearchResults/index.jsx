18bdcc186834cf3b062e16844770df1cd2ffce5a
prantoshikder <prantoshikder24@gmail.com>
2021-08-28 18:53:10 +0600

    Added search results component

---
+++ src/components/ui/SearchResults/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Footer from '../Footer';
+import Header from '../Header';
+
+const SearchResults = () => {
+    return (
+        <div>
+            <Header></Header>
+            <h1>SearchResults</h1>
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default SearchResults;
