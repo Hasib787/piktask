1538d3a126ccddfbd077f3bdc77722db4086be0a
Md. Masud Rana <mr.hasib45@gmail.com>
2021-09-08 18:37:43 +0600

    added a product reducer

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,6 +1,7 @@
 import { combineReducers } from "redux";
 import { categoriesReducer } from "./categoriesReducer";
 import { categoryReducer } from "./categoryReducer";
+import { productReducer } from "./productReducer";
 import { recentPhotoReducer } from "./recentPhotoReducer";
 import { userReducer } from "./userReducer";
 
@@ -9,6 +10,7 @@ const rootReducer = combineReducers({
   popularCategories: categoryReducer,
   recentPhotos: recentPhotoReducer,
   allCategories: categoriesReducer,
+  productCategories: productReducer,
 });
 
 export default rootReducer;
