2b98f33eef1958f2c82dfba1910dcdf03d1c76b7
prantoshikder <prantoshikder24@gmail.com>
2021-09-29 12:15:28 +0600

    Blogs comments API integration

--- src/pages/SingleBlogPost/SinglePost.styles.js
+++ src/pages/SingleBlogPost/SinglePost.styles.js
@@ -14,6 +14,53 @@ const useStyles = makeStyles((theme) => ({
       objectFit: "cover",
     },
   },
+  fieldWrapper: {
+    marginBottom: "1.4rem",
+    "& label": {
+      marginBottom: "0.5rem",
+    },
+    "& label > span": {
+      color: "red",
+    },
+  },
+  formDescription: {
+    position: 'relative',
+    width: "100%",
+    maxWidth: "100%",
+    fontSize: "1.5rem",
+    padding: "1rem",
+    borderRadius: "5px",
+    boxShadow: "0 8px 12px 3px rgb(0 0 0 / 6%)",
+    // border: "0",
+    borderColor: "#ddd",
+
+    "& .MuiInputLabel-outlined.Mui-focused": {
+      border: "5px solid",
+      borderColor: 'yellow !important',
+      color: "yellow",
+    },
+
+  },
+  sentBtn: {
+    padding: "0.8rem 3.5rem",
+    border: "none",
+    borderRadius: "10rem",
+    fontSize: "1.4rem",
+    cursor: "pointer",
+    backgroundColor: "#0088f2",
+    color: "white",
+    transition: "all 0.3s linear",
+    "&:hover": {
+      backgroundColor: "#0773c5",
+    },
+    [theme.breakpoints.down(480)]: {
+      width: "100%",
+      marginLeft: "0%",
+      fontSize: "14px",
+      padding: "1rem 0.9rem",
+      transform: "translateX(0%)",
+    },
+  },
   blogAuthorInfo: {
     color: "#000"
   },
