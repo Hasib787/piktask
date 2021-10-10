0a69497e274d242d8555b67bf23525e3652d1f19
Md. Masud Rana <mr.hasib45@gmail.com>
2021-10-10 12:23:19 +0600

    added design on about page

--- src/pages/AboutUs/AboutUs.style.js
+++ src/pages/AboutUs/AboutUs.style.js
@@ -1,22 +1,80 @@
 import { makeStyles } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
-    supportWrapper: {
-        padding: "0rem 20rem",
-        [theme.breakpoints.down("769")]:{
-            padding: "0 2rem",
-        },
-      },
-      supportTitle:{
-        color: "black !important",
-        fontSize: "2.2rem",
-      },
-      description: {
-        textAlign:"justify",
-        color: "#173050",
-        fontSize: "1.6rem",
-        lineHeight: "28px",
-      },
+  supportWrapper: {
+    padding: "0rem 20rem",
+    [theme.breakpoints.down("1200")]: {
+      padding: "0 7rem",
+    },
+    [theme.breakpoints.down("769")]: {
+      padding: "0 2rem",
+    },
+  },
+  aboutTitle: {
+    color: "black !important",
+    fontSize: "2.2rem",
+  },
+  description: {
+    paddingRight: "2rem",
+    textAlign: "justify",
+    color: "#173050",
+    fontSize: "1.6rem",
+    lineHeight: "28px",
+    [theme.breakpoints.down(769)]: {
+      paddingRight: "0rem",
+    },
+  },
+  meetingImage: {
+    width: "53rem",
+    "& img": {
+      paddingTop: "0.7rem",
+      width: "100%",
+      height: "100%",
+      objectFit: "cover",
+    },
+    [theme.breakpoints.down("1200")]: {
+      width: "100%",
+    },
+    [theme.breakpoints.down(769)]: {
+      width: "100%",
+      paddingTop: "2rem",
+    },
+  },
+  historyTitle: {
+    display: "flex",
+    color: "black !important",
+    fontSize: "2.2rem",
+  },
+  borderLine: {
+    height: "26.5rem",
+    marginTop: "0.7rem",
+    paddingLeft: "16rem",
+    borderRight: "0.7rem solid #8bc34a",
+    [theme.breakpoints.down("1200")]: {
+      paddingLeft: "8rem",
+    },
+    [theme.breakpoints.down(769)]: {
+      display: "none",
+    },
+  },
+  historyDescription: {
+    textAlign: "justify",
+    color: "#173050",
+    fontSize: "1.6rem",
+    lineHeight: "28px",
+    [theme.breakpoints.down(769)]: {
+      paddingTop: "1rem",
+    },
+  },
+  bdtaskMembersImage:{
+    width: "100%",
+    "& img": {
+      paddingTop: "0.7rem",
+      width: "100%",
+      height: "100%",
+      objectFit: "cover",
+    },
+  }
 }));
 
 export default useStyles;
